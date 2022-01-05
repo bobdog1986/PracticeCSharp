b06da5c39a4f33d907d24e67780a13904f580aa6
bobdog1986 <bobdog1986@126.com>
2022-01-05 23:30:36 +0800

    746

--- LeetCodeAlgo/Anwser.700.cs
+++ LeetCodeAlgo/Anwser.700.cs
@@ -64,6 +64,75 @@ namespace LeetCodeAlgo
             return true;
         }
 
+        //746. Min Cost Climbing Stairs
+        public int MinCostClimbingStairs(int[] cost)
+        {
+            if (cost == null || cost.Length == 0) return 0;
 
+            if (cost.Length == 1) return cost[0];
+
+            int[] dp = new int[cost.Length];
+
+            dp[0] = cost[0];
+            dp[1] = cost[1];
+
+            for (int i = 2; i < cost.Length; i++)
+            {
+                //every point cost must to last1 or last2
+                dp[i] = cost[i] + Math.Min(dp[i - 2], dp[i - 1]);
+            }
+
+            //every solution must to last1 or last2
+            return Math.Min(dp[cost.Length - 1], dp[cost.Length - 2]);
+        }
+
+
+        public int MinCostClimbingStairs_BruteForce(int[] cost)
+        {
+            if(cost == null || cost.Length == 0) return 0;
+
+            if(cost.Length == 1)return cost[0];
+
+            if(cost.Length ==2 )return Math.Min( cost[0],cost[1]);
+
+            int minCost = int.MaxValue;
+
+            //arr[0] as Index, arr[1] as totalCost
+            Queue<int[]> nodes = new Queue<int[]>();
+            nodes.Enqueue(new int[] { 0, cost[0] });
+            nodes.Enqueue(new int[] { 1, cost[1] });
+
+            while(nodes.Count > 0)
+            {
+                var node = nodes.Dequeue();
+                if (node[0] < cost.Length - 2)
+                {
+                    if((node[1] + cost[node[0] + 1])< minCost)
+                    {
+                        nodes.Enqueue(new int[] { node[0] + 1, node[1] + cost[node[0] + 1] });
+
+                    }
+                    if (node[1] + cost[node[0] + 2] < minCost)
+                    {
+                        nodes.Enqueue(new int[] { node[0] + 2, node[1] + cost[node[0] + 2] });
+
+                    }
+                }
+                else
+                {
+                    if(node[1] < minCost)
+                    {
+                        minCost = node[1];
+                    }
+                }
+            }
+
+            return minCost;
+        }
+
+        public int GetMinCost(int n)
+        {
+            return Math.Min(n,int.MaxValue);
+        }
     }
 }
--- LeetCodeAlgo/Program.cs
+++ LeetCodeAlgo/Program.cs
@@ -13,16 +13,16 @@ namespace LeetCodeAlgo
         {
             Console.WriteLine("Run\r\n****************************\r\n");
             var anwser = new Anwser();
-            var arr1 = new int[] { 1,2,3,0,0,0 };
-            var arr2 = new int[] { 2,5,6 };
+            var arr1 = new int[] { 1, 100, 1, 1, 1, 100, 1, 1, 100, 1 };
+            //var arr2 = new int[] { 2,5,6 };
 
             Console.WriteLine(string.Join(",", arr1));
-            Console.WriteLine(string.Join(",", arr2));
+            //Console.WriteLine(string.Join(",", arr2));
             Stopwatch sw = new Stopwatch();
             sw.Start();
             //[3,9,20,null,null,15,7]
             var root =new TreeNode(3,new TreeNode(9),new TreeNode(20,new TreeNode(15),new TreeNode(7)));
-            var result = anwser.ClimbStairs(4);
+            var result = anwser.MinCostClimbingStairs(arr1);
             //anwser.Merge(arr1,3,arr2,3);
             sw.Stop();
             Console.WriteLine("stop watch ms = " + sw.ElapsedMilliseconds);
