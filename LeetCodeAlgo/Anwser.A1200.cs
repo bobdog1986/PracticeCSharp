59f7542852b96a2fdf533ba88152fc60c9ac1e5b
bobdog1986 <bobdog1986@126.com>
2022-01-23 16:43:40 +0800

    1291. Sequential Digits
    An integer has sequential digits if and only if each digit in the number is one more than the previous digit.
    eg. 123, 234, 3456,

---
+++ LeetCodeAlgo/Anwser.A1200.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeAlgo
+{
+    public partial class Anwser
+    {
+        ///1291. Sequential Digits
+        ///An integer has sequential digits if and only if each digit in the number is one more than the previous digit.
+        ///eg. 123, 234, 3456,
+        public IList<int> SequentialDigits(int low, int high)
+        {
+            var ans=new List<int>();
+            int len1 = 1;
+            int len2 = 1;
+            int a = low;
+            int b = high;
+
+            while (a >= 10)
+            {
+                len1++;
+                a /= 10;
+            }
+
+            while (b >= 10)
+            {
+                len2++;
+                b /= 10;
+            }
+
+            for(int i =len1;i<=len2 && i<=9 ;i++)
+            {
+                int start = 1;
+                int end = 9;
+
+                if (i == len1)
+                    start = a;
+                if(i==len2)
+                    end = b;
+
+                for (int j = start; j <= 9 - i + 1 && j<=end ; j++)
+                {
+                    int val = 0;
+                    int m = i;
+                    int n = j;
+                    while (m > 0)
+                    {
+                        val *= 10;
+                        val += n;
+
+                        m--;
+                        n++;
+                    }
+
+                    if (val >= low && val <= high)
+                        ans.Add(val);
+                }
+            }
+
+            return ans;
+        }
+    }
+}
