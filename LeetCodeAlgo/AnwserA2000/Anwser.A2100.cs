24ab8edc9a5906b4c104b307bfa4f04ee14361b7
liang.gao <liang.gao@we-med.com>
2022-01-29 16:18:23 +0800

    2148. Count Elements With Strictly Smaller and Greater Elements
    return the number of elements that have both a strictly smaller and a strictly greater element appear in nums.

--- LeetCodeAlgo/AnwserA2000/Anwser.A2100.cs
+++ LeetCodeAlgo/AnwserA2000/Anwser.A2100.cs
@@ -1,4 +1,8 @@
-﻿namespace LeetCodeAlgo
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCodeAlgo
 {
     public partial class Anwser
     {
@@ -8,5 +12,27 @@
         {
             return num == 0 || num % 10 != 0;
         }
+        ///2148. Count Elements With Strictly Smaller and Greater Elements
+        ///return the number of elements that have both a strictly smaller and a strictly greater element appear in nums.
+        ///-100000 <= nums[i] <= 100000
+        public int CountElements(int[] nums)
+        {
+            int ship = 100000;
+            int[] arr=new int[ship*2+1];
+            int start = arr.Length-1;
+            int end = 0;
+            foreach(var num in nums)
+            {
+                var index = num + ship;
+                arr[index]++;
+                start=Math.Min(start, index);
+                end = Math.Max(end, index);
+            }
+            int sum = 0;
+            for (int i = start+1; i <= end-1; i++)
+                sum += arr[i];
+            return sum;
+        }
+
     }
 }
