f0c118d84251ff04a59eaf15008f4c6a84509710
bobdog1986 <bobdog1986@126.com>
2022-02-04 00:39:11 +0800

    997. Find the Town Judge

---
+++ LeetCodeAlgo/Anwser500/Anwser.950.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeAlgo
+{
+    public partial class Anwser
+    {
+        ///997. Find the Town Judge
+        ///In a town, there are n people labeled from 1 to n. There is a rumor that one of these people is secretly the town judge.
+        ///The town judge trusts nobody.Everybody (except for the town judge) trusts the town judge.
+        ///Return the label of the town judge if the town judge exists and can be identified, or return -1 otherwise.
+        public int FindJudge(int n, int[][] trust)
+        {
+            int[] beTrustedArr = new int[n+1];
+            int[] trustArr = new int[n+1];
+
+            foreach(var t in trust)
+            {
+                beTrustedArr[t[1]]++;
+                trustArr[t[0]]++;
+            }
+
+            for(int i = 1; i <= n; i++)
+            {
+                if(beTrustedArr[i]==n-1 && trustArr[i]==0)
+                    return i;
+            }
+
+            return -1;
+        }
+    }
+}
