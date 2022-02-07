992eb4d356f56175c61a61443650130f936c83ef
liang.gao <liang.gao@we-med.com>
2022-02-07 13:15:48 +0800

    698. Partition to K Equal Sum Subsets
    return true if it is possible to divide this array into k non-empty subsets whose sums are all equal.

---
+++ LeetCodeAlgo/Anwser500/Anwser.650.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeAlgo
+{
+    public partial class Anwser
+    {
+        ///698. Partition to K Equal Sum Subsets
+        ///return true if it is possible to divide this array into k non-empty subsets whose sums are all equal.
+        ///1 <= k <= nums.length <= 16,1 <= nums[i] <= 10^4, The frequency of each element is in the range [1, 4].
+        public bool CanPartitionKSubsets(int[] nums, int k)
+        {
+            if (k == 1)
+                return true;
+
+            if (k == nums.Length)
+            {
+                if (nums.Distinct().Count() != 1)
+                {
+                    return false;
+                }
+            }
+
+            int sum = nums.Sum();
+            if (sum % k != 0)
+                return false;
+
+            int goal = sum / k;
+
+            bool[] visit=new bool[nums.Length];
+            foreach(var n in nums)
+            {
+                if (n > goal)
+                    return false;
+            }
+
+            nums=nums.OrderBy(x => -x).ToArray();
+            bool found = false;
+            CanPartitionKSubsets_Recursion(nums, visit, 0, k, goal, 0, ref found);
+            return found;
+        }
+
+        public void CanPartitionKSubsets_Recursion(int[] nums, bool[] visit, int left, int k, int goal, int sum, ref bool found)
+        {
+            if (found == true)
+                return;
+
+            if (k == 1)
+            {
+                found = true;
+                return;
+            }
+
+            for (int i = left; i < nums.Length; i++)
+            {
+                if (visit[i])
+                    continue;
+
+                if (nums[i] == goal-sum)
+                {
+                    var nextVisit=new bool[nums.Length];
+                    Array.Copy(visit, nextVisit, visit.Length);
+                    nextVisit[i] = true;
+                    CanPartitionKSubsets_Recursion(nums, nextVisit, 0, k - 1, goal, 0, ref found);
+                }
+                else if(nums[i] < goal-sum)
+                {
+                    var nextVisit = new bool[nums.Length];
+                    Array.Copy(visit, nextVisit, visit.Length);
+                    nextVisit[i] = true;
+                    CanPartitionKSubsets_Recursion(nums, nextVisit, i+1, k, goal, sum+ nums[i], ref found);
+                }
+            }
+        }
+
+    }
+}
--- LeetCodeAlgo/Program.cs
+++ LeetCodeAlgo/Program.cs
@@ -12,7 +12,7 @@ namespace LeetCodeAlgo
             Console.WriteLine("Run\r\n****************************\r\n");
             var anwser = new Anwser();
             //var arr1 = new int[] { 5, 7, -24, 12, 13, 2, 3, 12, 5, 6, 35 };//[[},new int[]{],[]] should 6
-            var arr1 = new int[] { 1, 3, 5, 4, 7 };
+            var arr1 = new int[] { 3, 3, 10, 2, 6, 5, 10, 6, 8, 3, 2, 1, 6, 10, 7, 2};
             var arr2 = new int[] { -1, 2 };
             var val1 = -2;
             var val2 = 2;
@@ -58,19 +58,19 @@ namespace LeetCodeAlgo
                 //new char[]{'0','0','0'},
             };
 
-            var listnode = anwser.buildListNode(new int[] { 1, 3, 2, -3, -2, 5, 5, -5, 1});
+            //var listnode = anwser.buildListNode(new int[] { 1, 3, 2, -3, -2, 5, 5, -5, 1});
             //anwser.PrintListNode(listnode);
-            Console.WriteLine("Correct Anwser should be : ");
+            //Console.WriteLine("Correct Anwser should be : ");
             //Console.WriteLine(string.Join("\r\n", mat1.Select(o => string.Join(",", o))));
             //Console.WriteLine(string.Join(",", arr2));
             Stopwatch sw = new Stopwatch();
             Console.WriteLine("**************start watch ms*******");
             sw.Start();
-            string treeStr = "1,2,2,3,null,null,3,4,null,null,null,null,null,null,4";
-            var tree3 = anwser.deserializeTree(treeStr);
-            //var str3= anwser.serializeTree(tree3);
-            anwser.printTree(tree3);
-            //var result = anwser.NumDecodings("111111111111111111111111111111111111111111111");
+            //string treeStr = "1,2,2,3,null,null,3,4,null,null,null,null,null,null,4";
+            //var tree3 = anwser.deserializeTree(treeStr);
+            ////var str3= anwser.serializeTree(tree3);
+            //anwser.printTree(tree3);
+
             //Console.WriteLine(String.Join(",", arr1));
             //if (grid1.Length > 0)
             //    Console.WriteLine(String.Join("\r\n", grid1.Select(o => String.Join(",", o))));
@@ -79,7 +79,7 @@ namespace LeetCodeAlgo
 
             //anwser.SolveSudoku(grid1);
 
-            var result = anwser.IsBalanced(tree3);
+            var result = anwser.CanPartitionKSubsets(arr1,6);
             //var result = anwser.getFactorial(20);
             //var result2 = anwser.IsSameAfterReversals(0);
             sw.Stop();
