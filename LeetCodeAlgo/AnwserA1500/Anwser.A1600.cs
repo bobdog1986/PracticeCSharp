2766e7b501457622ae7e9218d00a321e6f0cb609
liang.gao <liang.gao@we-med.com>
2022-02-07 14:03:58 +0800

    1694. Reformat Phone Number
    number consists of digits, spaces ' ', and/or dashes '-'.

--- LeetCodeAlgo/AnwserA1500/Anwser.A1600.cs
+++ LeetCodeAlgo/AnwserA1500/Anwser.A1600.cs
@@ -34,10 +34,51 @@ namespace LeetCodeAlgo
             return len == -1 ? -1 : nums.Length - len;
         }
         ///1672.Richest Customer Wealth
-
         public int MaximumWealth(int[][] accounts)
         {
             return accounts.Select(x => x.Sum()).Max();
+        }
+
+        ///1694. Reformat Phone Number
+        ///number consists of digits, spaces ' ', and/or dashes '-'.
+        ///Firstly, remove all spaces and dashes.
+        ///Then, group the digits from left to right into blocks of length 3 until there are 4 or fewer digits.
+        ///The final digits are then grouped as follows:
+        /// 2 digits: A single block of length 2.
+        /// 3 digits: A single block of length 3.
+        /// 4 digits: Two blocks of length 2 each.
+        public string ReformatNumber(string number)
+        {
+            List<char> list=new List<char>();
+            foreach(var n in number)
+            {
+                if (n >= '0' && n <= '9')
+                {
+                    list.Add(n);
+                }
+            }
+            return ReformatNumber(list, list.Count);
+        }
+
+        public string ReformatNumber(List<char> list, int count)
+        {
+            if(count == 2)
+            {
+                return new String(new char[] { list[list.Count - 2], list[list.Count - 1] });
+            }
+            else if(count == 3)
+            {
+                return new String(new char[] { list[list.Count - 3], list[list.Count - 2], list[list.Count - 1] });
+            }
+            else if(count ==4)
+            {
+                return new String(new char[] { list[list.Count - 4], list[list.Count - 3], '-',list[list.Count - 2], list[list.Count - 1] });
+            }
+            else
+            {
+                return new String(new char[] { list[list.Count - count], list[list.Count - count + 1], list[list.Count - count + 2] })
+                    + "-" + ReformatNumber(list, count - 3);
+            }
         }
     }
 }
