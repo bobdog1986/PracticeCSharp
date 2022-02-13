44cb3e0a16d94ebd3b976e758d54f4a2accf6eb3
bobdog1986 <bobdog1986@126.com>
2022-02-13 22:39:25 +0800

    1758. Minimum Changes To Make Alternating Binary String
    Return the minimum number of operations needed to make s alternating. 010101 or 101010

---
+++ LeetCodeAlgo/AnwserA1500/Anwser.A1750.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeAlgo
+{
+    public partial class Anwser
+    {
+        /// 1758. Minimum Changes To Make Alternating Binary String
+        /// Return the minimum number of operations needed to make s alternating. 010101 or 101010
+        public int MinOperations(string s)
+        {
+            int dp0 = 0;
+            //int dp1 = 0;
+            //char c0 = '0';
+            //char c1 = '1';
+            for(int i=0; i<s.Length; i++)
+            {
+                if(s[i]-'0'!=i%2)
+                    dp0 ++;
+                //if (s[i] != c1)
+                    //dp1++;
+                //var temp = c0;
+                //c0 = c1;
+                //c1 = temp;
+            }
+            return Math.Min(dp0, s.Length-dp0);
+        }
+    }
+}
