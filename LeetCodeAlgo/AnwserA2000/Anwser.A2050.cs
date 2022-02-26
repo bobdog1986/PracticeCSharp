d30ddec71036728762a4efc00460697d723d7469
bobdog1986 <bobdog1986@126.com>
2022-02-26 14:57:40 +0800

    2068. Check Whether Two Strings are Almost Equivalent
    Given two strings word1 and word2, each of length n, return true if word1 and word2 are almost equivalent or false differences between the frequencies of each letter from 'a' to 'z' between word1 and word2 is at most 3.

--- LeetCodeAlgo/AnwserA2000/Anwser.A2050.cs
+++ LeetCodeAlgo/AnwserA2000/Anwser.A2050.cs
@@ -8,7 +8,23 @@ namespace LeetCodeAlgo
 {
     public partial class Anwser
     {
-        ///2086. Minimum Number of Buckets Required to Collect Rainwater from Houses
+        ///2068. Check Whether Two Strings are Almost Equivalent
+        ///Given two strings word1 and word2, each of length n, return true if word1 and word2 are almost equivalent or false
+        ///differences between the frequencies of each letter from 'a' to 'z' between word1 and word2 is at most 3.
+
+        public bool CheckAlmostEquivalent(string word1, string word2)
+        {
+            int[] arr=new int[26];
+            for(int i=0; i<word1.Length; i++)
+            {
+                arr[word1[i] - 'a']++;
+                arr[word2[i] - 'a']--;
+            }
+            foreach(var x in arr)
+                if (x > 3 || x < -3) return false;
+            return true;
+        }
+        /// 2086. Minimum Number of Buckets Required to Collect Rainwater from Houses
         ///H is house, . is space
         ///The rainwater from a house at index i is collected if a bucket is placed at index i - 1 and/or index i + 1.
         ///Return the minimum number of buckets for every house at least one bucket collecting rainwater, or -1 if it is impossible.
