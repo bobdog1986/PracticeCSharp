6dea724a53767662983788ce9924fb505ae83e04
liang.gao <liang.gao@we-med.com>
2022-03-01 15:11:23 +0800

    1759. Count Number of Homogenous Substrings
    Given a string s, return the number of homogenous substrings of s.Since the answer may be too large, return it modulo 109 + 7.A string is homogenous if all the characters of the string are the same.

--- LeetCodeAlgo/AnswerA1500/Answer.A1750.cs
+++ LeetCodeAlgo/AnswerA1500/Answer.A1750.cs
@@ -63,5 +63,47 @@ namespace LeetCodeAlgo
             }
             return Math.Min(dp0, s.Length-dp0);
         }
+
+        ///1759. Count Number of Homogenous Substrings
+        ///Given a string s, return the number of homogenous substrings of s.
+        ///Since the answer may be too large, return it modulo 109 + 7.
+        ///A string is homogenous if all the characters of the string are the same.
+        public int CountHomogenous(string s)
+        {
+            long ans = 0;
+            long mod = 10_0000_0007;
+            Dictionary<long, long> dict = new Dictionary<long, long>();
+            char c = s[0];
+            long count = 1;
+            for (int i = 1; i < s.Length; i++)
+            {
+                if (c == s[i]) { count++; }
+                else
+                {
+                    if (!dict.ContainsKey(count)) { CountHomogenous(count, dict); }
+                    ans+= dict[count];
+                    ans %= mod;
+                    c = s[i];
+                    count = 1;
+                }
+            }
+            if (!dict.ContainsKey(count)) { CountHomogenous(count, dict); }
+            ans += dict[count];
+            ans %= mod;
+            return (int)(ans % mod);
+        }
+        public void CountHomogenous(long count, Dictionary<long,long> dict)
+        {
+            long ans = 0;
+            long seed = 0;
+            int i = 0;
+            while (i<=count)
+            {
+                ans += seed;
+                if (!dict.ContainsKey(i)) dict.Add(i, ans);
+                i++ ;
+                seed++;
+            }
+        }
     }
 }
