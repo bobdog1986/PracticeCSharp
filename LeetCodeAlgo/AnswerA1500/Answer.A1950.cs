8ce485394c1b9c6c417a582d3dba9ed0a85f4dd3
liang.gao <liang.gao@we-med.com>
2022-03-02 16:46:29 +0800

    1952. Three Divisors
    n has exactly 3 divisors, 1 <= n <= 10^4

--- LeetCodeAlgo/AnswerA1500/Answer.A1950.cs
+++ LeetCodeAlgo/AnswerA1500/Answer.A1950.cs
@@ -8,7 +8,30 @@ namespace LeetCodeAlgo
 {
     public partial class Answer
     {
-        ///1961. Check If String Is a Prefix of Array
+        ///1952. Three Divisors
+        ///n has exactly 3 divisors, 1 <= n <= 10^4
+        public bool IsThree(int n)
+        {
+            //get primes <=100, https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes
+            int[] arr = new int[101];
+            for(int i = 2; i*i <= 100; i++)
+            {
+                int j = i+i;
+                while (j <= 100)
+                {
+                    arr[j] = 1;
+                    j += i;
+                }
+            }
+            if (n > 2)
+            {
+                var sqrt = (int)Math.Sqrt(n);
+                if(arr[sqrt]==0 && sqrt* sqrt==n)return true;
+                else return false;
+            }
+            else return false;
+        }
+        /// 1961. Check If String Is a Prefix of Array
         ///Given a string s and an array of strings words, determine whether s is a prefix string of words.
         ///A string s is a prefix string of words if s can be made by concatenating the first k strings in words
         public bool IsPrefixString(string s, string[] words)
