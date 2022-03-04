0739f68b0a881c2685de23f8dda5c8547fd520b1
liang.gao <liang.gao@we-med.com>
2022-03-04 17:59:28 +0800

    2185. Counting Words With a Given Prefix

---
+++ Source/LeetCodeAlgo/AnswerA2000/Answer.A2150.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeAlgo
+{
+    public partial class Answer
+    {
+        ///2185. Counting Words With a Given Prefix
+        public int PrefixCount(string[] words, string pref)
+        {
+            return words.Where(word => word.StartsWith(pref)).Count();
+        }
+    }
+}
