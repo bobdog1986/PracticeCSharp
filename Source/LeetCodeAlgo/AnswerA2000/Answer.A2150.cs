eb4b9dc3d67f5ce4c63b9447c529d6d2787a67b5
bobdog1986 <bobdog1986@126.com>
2022-03-06 00:24:53 +0800

    2190. Most Frequent Number Following Key In an Array
    0 <= i <= n - 2, nums[i] == key and, nums[i + 1] == target. Return the target with the maximum count.

--- Source/LeetCodeAlgo/AnswerA2000/Answer.A2150.cs
+++ Source/LeetCodeAlgo/AnswerA2000/Answer.A2150.cs
@@ -13,5 +13,29 @@ namespace LeetCodeAlgo
         {
             return words.Where(word => word.StartsWith(pref)).Count();
         }
+
+        ///2190. Most Frequent Number Following Key In an Array
+        ///0 <= i <= n - 2, nums[i] == key and, nums[i + 1] == target.
+        ///Return the target with the maximum count.
+        public int MostFrequent(int[] nums, int key)
+        {
+            int max = 0;
+            int res = 0;
+            Dictionary<int, int> map = new Dictionary<int, int>();
+            for(int i = 0; i < nums.Length-1; i++)
+            {
+                if(nums[i] == key)
+                {
+                    if(map.ContainsKey(nums[i+1]))map[nums[i+1]]++;
+                    else map.Add(nums[i+1], 1);
+                    if (map[nums[i + 1]] > max)
+                    {
+                        max = map[nums[i+1]];
+                        res = nums[i + 1];
+                    }
+                }
+            }
+            return res;
+        }
     }
 }
