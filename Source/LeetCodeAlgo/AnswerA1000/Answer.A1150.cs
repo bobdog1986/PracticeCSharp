4a765205ed67aea54a78e7d616f71f74240dabf2
bobdog1986 <bobdog1986@126.com>
2022-03-06 01:41:34 +0800

    1162. As Far from Land as Possible, #Graph, #DP, #BFS
    0 represents water and 1 represents land, find a water cell such that its distance to the nearest land cell is maximized, and return the distance. If no land or water exists in the grid, return -1.

--- Source/LeetCodeAlgo/AnswerA1000/Answer.A1150.cs
+++ Source/LeetCodeAlgo/AnswerA1000/Answer.A1150.cs
@@ -9,7 +9,83 @@ namespace LeetCodeAlgo
     public partial class Answer
     {
 
-        ///1171. Remove Zero Sum Consecutive Nodes from Linked List
+        ///1162. As Far from Land as Possible, #Graph, #DP, #BFS
+        ///0 represents water and 1 represents land,
+        ///find a water cell such that its distance to the nearest land cell is maximized,
+        ///and return the distance. If no land or water exists in the grid, return -1.
+        public int MaxDistance1162_BFS(int[][] grid)
+        {
+            int rowLen=grid.Length;
+            int colLen=grid[0].Length;
+            bool[,] visited = new bool[rowLen, colLen];
+            int[][] dxy4 = new int[4][] { new int[] { 0, 1 }, new int[] { 0, -1 }, new int[] { 1, 0 }, new int[] { -1, 0 } };
+
+            Queue<int[]> queue = new Queue<int[]>();
+            for (int i = 0; i < rowLen; i++)
+            {
+                for (int j = 0; j < colLen; j++)
+                {
+                    if (grid[i][j] == 1)
+                    {
+                        queue.Enqueue(new int[] { i, j });
+                        visited[i,j] = true;
+                    }
+                }
+            }
+            int level = -1;
+            while (queue.Count>0)
+            {
+                int size = queue.Count;
+                for (int i = 0; i < size; i++)
+                {
+                    int[] p = queue.Dequeue();
+                    foreach (var  d in dxy4)
+                    {
+                        int r = p[0] + d[0];
+                        int c = p[1] + d[1];
+                        if (r >= 0 && r < rowLen && c >= 0 && c < colLen
+                           && !visited[r,c] && grid[r][c] == 0)
+                        {
+                            visited[r,c] = true;
+                            queue.Enqueue(new int[] { r, c });
+                        }
+                    }
+                }
+                level++;
+            }
+            return level <= 0 ? -1 : level;
+        }
+
+        public int MaxDistance1162_DP(int[][] grid)
+        {
+            int n = grid.Length, m = grid[0].Length, res = 0;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    if (grid[i][j] == 1) continue;
+                    grid[i][j] = 201; //201 here cuz as the despription, the size won't exceed 100.
+                    if (i > 0) grid[i][j] = Math.Min(grid[i][j], grid[i - 1][j] + 1);
+                    if (j > 0) grid[i][j] = Math.Min(grid[i][j], grid[i][j - 1] + 1);
+                }
+            }
+
+            for (int i = n - 1; i > -1; i--)
+            {
+                for (int j = m - 1; j > -1; j--)
+                {
+                    if (grid[i][j] == 1) continue;
+                    if (i < n - 1) grid[i][j] = Math.Min(grid[i][j], grid[i + 1][j] + 1);
+                    if (j < m - 1) grid[i][j] = Math.Min(grid[i][j], grid[i][j + 1] + 1);
+                    res = Math.Max(res, grid[i][j]); //update the maximum
+                }
+            }
+
+            return res == 201 ? -1 : res - 1;
+        }
+
+
+        /// 1171. Remove Zero Sum Consecutive Nodes from Linked List
         public ListNode RemoveZeroSumSublists(ListNode head)
         {
             var node = head;
