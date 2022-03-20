1229f0c2bb3e2eee625766f8f9cb25aefe70c903
bobdog1986 <bobdog1986@126.com>
2022-03-20 22:36:18 +0800

    2196. Create Binary Tree From Descriptions
    descriptions[i] = [parenti, childi, isLefti] indicates that parenti is the parent of childi in a binary tree of unique values. Construct the binary tree described by descriptions and return its root.

--- Source/LeetCodeAlgo/Answer0/Answer.0.cs
+++ Source/LeetCodeAlgo/Answer0/Answer.0.cs
@@ -10,8 +10,6 @@ namespace LeetCodeAlgo
     {
         /// 1. Two Sum, #HashMap, #Two Pointers
         /// return indices of the two numbers such that they add up to target.
-        /// each input would have exactly one solution, and you may not use the same element twice.
-        /// 2 <= nums.length <= 10^4, -10^9 <= nums[i] <= 10^9, -10^9 <= target <= 10^9
         public int[] TwoSum(int[] nums, int target)
         {
             Dictionary<int, int> dict = new Dictionary<int, int>();
@@ -31,8 +29,6 @@ namespace LeetCodeAlgo
         }
 
         ///2. Add Two Numbers
-        ///You are given two non-empty linked lists representing two non-negative integers.
-        ///The digits are stored in reverse order, and each of their nodes contains a single digit.
         ///Add the two numbers and return the sum as a linked list.
         ///Input: l1 = [2,4,3], l2 = [5,6,4]
         ///Output: [7,0,8]
@@ -102,8 +98,7 @@ namespace LeetCodeAlgo
         /// s consists of English letters, digits, symbols and spaces.
         public int LengthOfLongestSubstring(string s)
         {
-            if (s.Length <= 1)
-                return s.Length;
+            if (s.Length <= 1) return s.Length;
             int max = 1;
             int len = 0;
             int[] arr = new int[128];
--- Source/LeetCodeAlgo/AnswerA2000/Answer.A2150.cs
+++ Source/LeetCodeAlgo/AnswerA2000/Answer.A2150.cs
@@ -37,5 +37,35 @@ namespace LeetCodeAlgo
             }
             return res;
         }
+        ///2196. Create Binary Tree From Descriptions
+        ///descriptions[i] = [parenti, childi, isLefti] indicates that parenti is the parent of childi in a binary tree of unique values. Furthermore,
+        ///If isLefti == 1, then childi is the left child of parenti.
+        ///If isLefti == 0, then childi is the right child of parenti.
+        ///Construct the binary tree described by descriptions and return its root.
+        public TreeNode CreateBinaryTree(int[][] descriptions)
+        {
+            //set contains all nodes,but only the root's value is true
+            Dictionary<TreeNode, bool> set = new Dictionary<TreeNode, bool>();
+            Dictionary<int, TreeNode> dict = new Dictionary<int, TreeNode>();
+            foreach(var desc in descriptions)
+            {
+                TreeNode parent = null;
+                if (!dict.ContainsKey(desc[0])) dict.Add(desc[0], new TreeNode(desc[0]));
+                parent = dict[desc[0]];
+                if (!set.ContainsKey(parent)) set.Add(parent,true);
+
+
+                TreeNode child = null;
+                if (!dict.ContainsKey(desc[1])) dict.Add(desc[1], new TreeNode(desc[1]));
+                child = dict[desc[1]];
+                if (!set.ContainsKey(child)) set.Add(child, false);
+                else set[child] = false;
+
+                if (desc[2] == 1) parent.left = child;
+                else parent.right = child;
+            }
+
+            return set.FirstOrDefault(x => x.Value).Key;
+        }
     }
 }
