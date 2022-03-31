a609e1ea7ba0e73ff5239e9bf5389ba68c8f76e0
bobdog1986 <bobdog1986@126.com>
2022-04-01 00:50:34 +0800

    2215. Find the Difference of Two Arrays
    Given two 0-indexed integer arrays nums1 and nums2, return a list answer of size 2 where: answer[0] is a list of all distinct integers in nums1 which are not present in nums2. answer[1] is a list of all distinct integers in nums2 which are not present in nums1

---
+++ Source/LeetCodeAlgo/AnswerA2000/Answer.A2200.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeAlgo
+{
+    public partial class Answer
+    {
+        ///2215. Find the Difference of Two Arrays
+        ///Given two 0-indexed integer arrays nums1 and nums2, return a list answer of size 2 where:
+        ///answer[0] is a list of all distinct integers in nums1 which are not present in nums2.
+        ///answer[1] is a list of all distinct integers in nums2 which are not present in nums1
+
+        public IList<IList<int>> FindDifference(int[] nums1, int[] nums2)
+        {
+            var res = new List<IList<int>>();
+            HashSet<int> set1 = new HashSet<int>(nums1);
+            HashSet<int> set2 = new HashSet<int>(nums2);
+
+            res.Add(set1.Where(x => !set2.Contains(x)).ToList());
+            res.Add(set2.Where(x => !set1.Contains(x)).ToList());
+            return res;
+        }
+    }
+}
