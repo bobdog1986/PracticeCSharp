740bca81269483f99c9cf1fe7498487992ec6ae0
bobdog1986 <bobdog1986@126.com>
2022-04-07 22:03:20 +0800

    SQL 183,584,595,1757

---
+++ Source/LeetCodeAlgo/SqlScripts/script1.sql
@@ -0,0 +1,29 @@
+﻿
+/*
+///183. Customers Who Never Order
+
+select name as Customers from Customers where NOT EXISTS (SELECT * FROM Orders WHERE Customers.id=Orders.customerId)
+
+
+///584. Find Customer Referee
+
+select name from Customer where isnull(referee_id) || referee_id !=2
+
+///595. Big Countries
+
+select name,population, area from World where area>=3000000 or population>=25000000
+
+
+
+
+
+
+
+
+
+
+
+///1757. Recyclable and Low Fat Products
+
+select product_id   from Products where low_fats='Y' and recyclable ='Y'
+*/
