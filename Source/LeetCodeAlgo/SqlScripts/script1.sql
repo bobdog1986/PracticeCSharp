cc37369a725ab64041c18651a60de27f3fb27903
bobdog1986 <bobdog1986@126.com>
2022-04-08 23:08:00 +0800

    SQL 196, 627, 1873

--- Source/LeetCodeAlgo/SqlScripts/script1.sql
+++ Source/LeetCodeAlgo/SqlScripts/script1.sql
@@ -4,6 +4,13 @@
 
 select name as Customers from Customers where NOT EXISTS (SELECT * FROM Orders WHERE Customers.id=Orders.customerId)
 
+///196. Delete Duplicate Emails
+
+delete from Person where id not in(
+    select t.id from (
+        select min(id) as id from Person group by email
+    ) t
+)
 
 ///584. Find Customer Referee
 
@@ -15,7 +22,9 @@ select name,population, area from World where area>=3000000 or population>=25000
 
 
 
+///627. Swap Salary
 
+update salary set sex = CHAR(ASCII('f') ^ ASCII('m') ^ ASCII(sex));
 
 
 
@@ -26,4 +35,19 @@ select name,population, area from World where area>=3000000 or population>=25000
 ///1757. Recyclable and Low Fat Products
 
 select product_id   from Products where low_fats='Y' and recyclable ='Y'
+
+
+///1873. Calculate Special Bonus
+
+SELECT employee_id ,
+            CASE
+                WHEN mod(employee_id ,2)=1 and name not like 'M%'
+                    THEN salary
+                ELSE 0
+            END
+            as bonus FROM Employees ;
+
+
+
+
 */
