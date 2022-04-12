2b59cc3202104907386260242ab4f9c1bf2fbbcc
liang.gao <liang.gao@we-med.com>
2022-04-12 13:33:14 +0800

    1402. Reducing Dishes

--- Source/LeetCodeAlgo/AnswerA1000/Answer.A1400.cs
+++ Source/LeetCodeAlgo/AnswerA1000/Answer.A1400.cs
@@ -13,14 +13,40 @@ namespace LeetCodeAlgo
         {
             if (s.Length < k) return false;
             if (s.Length == k) return true;
-            Dictionary<char,int> dict=new Dictionary<char, int>();
-            foreach(var c in s)
+            Dictionary<char, int> dict = new Dictionary<char, int>();
+            foreach (var c in s)
             {
-                if(!dict.ContainsKey(c))dict.Add(c, 0);
+                if (!dict.ContainsKey(c)) dict.Add(c, 0);
                 dict[c]++;
             }
             var singleCount = dict.Where(x => x.Value % 2 == 1).Count();
             return k >= singleCount;
+        }
+
+        ///1402. Reducing Dishes
+        public int MaxSatisfaction(int[] satisfaction)
+        {
+            int res = 0;
+            PriorityQueue<int, int> pq = new PriorityQueue<int, int>();
+            foreach (var n in satisfaction)
+                pq.Enqueue(n, -n);
+
+            List<int> list = new List<int>();
+            int sum = 0;
+            while (pq.Count > 0)
+            {
+                var curr = pq.Dequeue();
+                if (curr >= 0 || curr + sum > 0)
+                {
+                    list.Insert(0, curr);
+                    sum += curr;
+                }
+                else break;
+            }
+
+            for (int i = 0; i < list.Count; i++)
+                res += (i + 1) * list[i];
+            return res;
         }
 
         ///1403. Minimum Subsequence in Non-Increasing Order
@@ -29,25 +55,26 @@ namespace LeetCodeAlgo
         {
             Array.Sort(nums);
             int total = nums.Sum();
-            var ans=new List<int>();
+            var ans = new List<int>();
             int sum = 0;
-            for(int i=nums.Length-1; i>=0; i--)
+            for (int i = nums.Length - 1; i >= 0; i--)
             {
-                sum+=nums[i];
+                sum += nums[i];
                 ans.Add(nums[i]);
                 if (sum > total - sum) break;
             }
             return ans;
         }
+
         ///1409. Queries on a Permutation With Key
         public int[] ProcessQueries(int[] queries, int m)
         {
-            int[] arr= new int[m];
+            int[] arr = new int[m];
             for (int i = 0; i < m; i++)
                 arr[i] = i + 1;
             var list = arr.ToList();
-            int[] res= new int[queries.Length];
-            for(int i=0;i<queries.Length;i++)
+            int[] res = new int[queries.Length];
+            for (int i = 0; i < queries.Length; i++)
             {
                 int index = list.IndexOf(queries[i]);
                 res[i] = index;
@@ -56,6 +83,7 @@ namespace LeetCodeAlgo
             }
             return res;
         }
+
         /// 1417. Reformat The String
         ///You are given an alphanumeric string s. Only lowercase English letters and digits.
         ///1 letter follow 1 digit , or reverse
@@ -66,26 +94,26 @@ namespace LeetCodeAlgo
             List<char> letters = new List<char>();
             List<char> digits = new List<char>();
 
-            foreach(var c in s)
+            foreach (var c in s)
             {
-                if(c>='0'&&c<='9')
+                if (c >= '0' && c <= '9')
                     digits.Add(c);
                 else
                     letters.Add(c);
             }
             if (letters.Count > digits.Count + 1 || digits.Count > letters.Count + 1)
                 return string.Empty;
-            if(letters.Count > digits.Count)
+            if (letters.Count > digits.Count)
             {
                 int i = 0;
-                for (; i< digits.Count; i++)
+                for (; i < digits.Count; i++)
                 {
                     ans.Add(letters[i]);
                     ans.Add(digits[i]);
                 }
                 ans.Add(letters[i]);
             }
-            else if(letters.Count < digits.Count)
+            else if (letters.Count < digits.Count)
             {
                 int i = 0;
                 for (; i < letters.Count; i++)
@@ -106,23 +134,24 @@ namespace LeetCodeAlgo
             }
             return string.Join("", ans);
         }
+
         /// 1422. Maximum Score After Splitting a String
         ///Split to 2 string,score is the number of zeros in the left + the number of ones in the right substring.
         public int MaxScore(string s)
         {
-            Dictionary<int, int> dict=new Dictionary<int, int>();
+            Dictionary<int, int> dict = new Dictionary<int, int>();
             int numOf0 = 0;
-            for(int i = 0; i < s.Length; i++)
+            for (int i = 0; i < s.Length; i++)
             {
-                if(s[i]=='0')
+                if (s[i] == '0')
                     numOf0++;
 
-                if(i<s.Length-1)
+                if (i < s.Length - 1)
                     dict.Add(i, numOf0);
             }
             int max = 0;
-            foreach(int key in dict.Keys)
-                max=Math.Max(max, dict[key]+s.Length-numOf0-(key+1- dict[key]));
+            foreach (int key in dict.Keys)
+                max = Math.Max(max, dict[key] + s.Length - numOf0 - (key + 1 - dict[key]));
             return max;
         }
 
@@ -131,18 +160,19 @@ namespace LeetCodeAlgo
         public bool KLengthApart(int[] nums, int k)
         {
             var ans = true;
-            int last = -k-1;
-            for(int i = 0; i < nums.Length; i++)
+            int last = -k - 1;
+            for (int i = 0; i < nums.Length; i++)
             {
                 if (nums[i] == 1)
                 {
-                    if(i- last <=k)
+                    if (i - last <= k)
                         return false;
                     last = i;
                 }
             }
             return ans;
         }
+
         ///1446. Consecutive Characters
         ///The power of the string is the maximum length of a non-empty substring that contains only one unique character.
         public int MaxPower(string s)
@@ -150,9 +180,9 @@ namespace LeetCodeAlgo
             int max = 0;
             int count = 1;
             char c = s[0];
-            for(int i = 1; i < s.Length; i++)
+            for (int i = 1; i < s.Length; i++)
             {
-                if(c == s[i]) { count++; }
+                if (c == s[i]) { count++; }
                 else
                 {
                     max = Math.Max(max, count);
@@ -163,6 +193,7 @@ namespace LeetCodeAlgo
             max = Math.Max(max, count);
             return max;
         }
+
         /// 1448. Count Good Nodes in Binary Tree
         ///a node X in the tree is named good if in the path from root to X there are no nodes with a value greater than X.
         public int GoodNodes(TreeNode root)
@@ -175,7 +206,8 @@ namespace LeetCodeAlgo
             GoodNodes_Recursion(root.right, max, ref ans);
             return ans;
         }
-        public void GoodNodes_Recursion(TreeNode node,int max, ref int ans)
+
+        public void GoodNodes_Recursion(TreeNode node, int max, ref int ans)
         {
             if (node == null)
                 return;
@@ -185,7 +217,5 @@ namespace LeetCodeAlgo
             GoodNodes_Recursion(node.left, max, ref ans);
             GoodNodes_Recursion(node.right, max, ref ans);
         }
-
-
     }
 }
