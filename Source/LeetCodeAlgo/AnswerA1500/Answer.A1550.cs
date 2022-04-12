69cdec1c0bef90e56f73ad68a85dd9026ef2bf28
liang.gao <liang.gao@we-med.com>
2022-04-12 13:17:25 +0800

    1551. Minimum Operations to Make Array Equal

--- Source/LeetCodeAlgo/AnswerA1500/Answer.A1550.cs
+++ Source/LeetCodeAlgo/AnswerA1500/Answer.A1550.cs
@@ -8,7 +8,17 @@ namespace LeetCodeAlgo
 {
     public partial class Answer
     {
-        ///1557. Minimum Number of Vertices to Reach All Nodes, #Graph
+        ///1551. Minimum Operations to Make Array Equal
+        public int MinOperations(int n)
+        {
+            int res = 0;
+            int left = 0;
+            int right = n - 1;
+            while(left < right)
+                res += right-- - left++;
+            return res;
+        }
+        /// 1557. Minimum Number of Vertices to Reach All Nodes, #Graph
         ///Given a directed acyclic graph, with n vertices numbered from 0 to n-1,
         ///and an array edges where edges[i] = [fromi, toi] represents a directed edge from node fromi to node toi.
         ///Find the smallest set of vertices from which all nodes in the graph are reachable.
