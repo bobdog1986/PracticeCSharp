cf653aa9985eb7cae5d4506c538922e5e70d771c
bobdog1986 <bobdog1986@users.noreply.github.com>
2022-05-01 12:08:19 +0800

    2259. Remove Digit From Number to Maximize Result

---
+++ Source/LeetCodeAlgo/AnswerA2000/Answer.A2250.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeAlgo.AnswerA2000
+{
+    public partial class Answer
+    {
+        ///2259. Remove Digit From Number to Maximize Result
+        public string RemoveDigit(string number, char digit)
+        {
+            string res = string.Empty;
+            for (int i = 0; i < number.Length; i++)
+            {
+                if (number[i] == digit)
+                {
+                    var str = number.Substring(0, i) + number.Substring(i + 1);
+                    if (string.IsNullOrEmpty(str) || string.Compare(res, str) < 0)
+                        res = str;
+                }
+            }
+            return res;
+        }
+
+
+    }
+}
