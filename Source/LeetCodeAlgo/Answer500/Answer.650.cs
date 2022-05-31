af1dea335a0e59a7670393bc04ff228131b47f70
bobdog1986 <bobdog1986@users.noreply.github.com>
2022-05-31 12:05:26 +0800

    696. Count Binary Substrings
    return the number of non-empty substrings that have the same number of 0's and 1's,
    and all the 0's and all the 1's in these substrings are grouped consecutively.

--- Source/LeetCodeAlgo/Answer500/Answer.650.cs
+++ Source/LeetCodeAlgo/Answer500/Answer.650.cs
@@ -521,7 +521,34 @@ namespace LeetCodeAlgo
             return max;
         }
 
-        ///697
+        ///696. Count Binary Substrings
+        //return the number of non-empty substrings that have the same number of 0's and 1's,
+        //and all the 0's and all the 1's in these substrings are grouped consecutively.
+        public int CountBinarySubstrings(string s)
+        {
+            //First, I count the number of 1 or 0 grouped consecutively.
+            //For example "0110001111" will be[1, 2, 3, 4].
+            //Second, for any possible substrings with 1 and 0 grouped consecutively,
+            //the number of valid substring will be the minimum number of 0 and 1.
+            //For example "0001111", will be min(3, 4) = 3, ("01", "0011", "000111")
+            int curr = 1;
+            int prev = 0;
+            int res = 0;
+            for (int i = 1; i < s.Length; i++)
+            {
+                if (s[i] == s[i - 1]) curr++;
+                else
+                {
+                    res += Math.Min(curr, prev);
+                    prev = curr;
+                    curr = 1;
+                }
+            }
+            res += Math.Min(curr, prev);
+            return res ;
+        }
+
+        /// 697
         public int FindShortestSubArray(int[] nums)
         {
             Dictionary<int, int> dictionary = new Dictionary<int, int>();
