9874b04d2a78626176044541b1ca8e9cb6266d86
bobdog1986 <bobdog1986@users.noreply.github.com>
2022-08-07 12:04:35 +0800

    2369. Check if There is a Valid Partition For The Array,#DP

--- Source/LeetCodeAlgo/AnswerA2000/Answer.A2350.cs
+++ Source/LeetCodeAlgo/AnswerA2000/Answer.A2350.cs
@@ -298,5 +298,24 @@ namespace LeetCodeAlgo
                 ReachableNodes(j, i, graph, set, res);
             }
         }
+
+        ///2369. Check if There is a Valid Partition For The Array,#DP
+        public bool ValidPartition(int[] nums)
+        {
+            int n = nums.Length;
+            bool[] dp = new bool[n+1];
+            dp[0] = true;
+            for(int i = 2; i <= n; i++)
+            {
+                if(nums[i - 1] == nums[i - 2])
+                    dp[i] |= dp[i - 2];
+                if (i>=3 && nums[i - 1] == nums[i - 2] && nums[i - 1] == nums[i - 3])
+                    dp[i] |= dp[i - 3];
+                if (i >= 3 && nums[i - 1] == nums[i - 2] +1 && nums[i - 1] == nums[i - 3]+2)
+                    dp[i] |= dp[i - 3];
+            }
+            return dp.Last();
+        }
+
     }
 }
