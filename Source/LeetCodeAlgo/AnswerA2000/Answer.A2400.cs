6517200b6792e5fb37298812084959e8f30eb814
bobdog1986 <bobdog1986@users.noreply.github.com>
2022-09-04 12:57:34 +0800

    2400. Number of Ways to Reach a Position After Exactly k Steps

---
+++ Source/LeetCodeAlgo/AnswerA2000/Answer.A2400.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeAlgo
+{
+    public partial class Answer
+    {
+        //2400. Number of Ways to Reach a Position After Exactly k Steps
+        public int NumberOfWays(int startPos, int endPos, int k)
+        {
+            long mod = 1_000_000_007;
+            Dictionary<int, long> dict = new Dictionary<int, long>();
+            dict.Add(startPos, 1);
+            while (k-- > 0)
+            {
+                var next = new Dictionary<int, long>();
+                foreach (var x in dict.Keys)
+                {
+                    if (!next.ContainsKey(x + 1))
+                        next.Add(x + 1, 0);
+                    next[x + 1] += dict[x];//move right
+                    next[x + 1] %= mod;
+
+                    if (!next.ContainsKey(x - 1))
+                        next.Add(x - 1, 0);
+                    next[x - 1] += dict[x];//move left
+                    next[x - 1] %= mod;
+                }
+                dict = next;
+            }
+            if (dict.ContainsKey(endPos)) return (int)dict[endPos];
+            else return 0;
+        }
+    }
+}
