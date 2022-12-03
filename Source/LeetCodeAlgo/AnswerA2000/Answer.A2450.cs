eada12cf1d5d87501e965818766b8c8b40841104
bobdog1986 <bobdog1986@users.noreply.github.com>
2022-12-03 13:34:27 +0800

    2485. Find the Pivot Integer

--- Source/LeetCodeAlgo/AnswerA2000/Answer.A2450.cs
+++ Source/LeetCodeAlgo/AnswerA2000/Answer.A2450.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -66,22 +67,47 @@ namespace LeetCodeAlgo
 
         ///2486. Append Characters to String to Make Subsequence
         //Return the minimum chars that need to be appended to the end of s so that t becomes a subsequence of s.
-        public int AppendCharacters(string s, string t)
+        //public int AppendCharacters(string s, string t)
+        //{
+        //    int i = 0, j = 0;
+        //    for(;i<s.Length && j<t.Length;)
+        //    {
+        //        if (s[i]==t[j])
+        //        {
+        //            i++;
+        //            j++;
+        //        }
+        //        else
+        //        {
+        //            i++;
+        //        }
+        //    }
+        //    return t.Length-j;
+        //}
+
+        ///2485. Find the Pivot Integer
+        //return x that sum of [1,x] == [x,n], if not exist, return -1
+        public int PivotInteger(int n)
         {
-            int i = 0, j = 0;
-            for(;i<s.Length && j<t.Length;)
+            int left = 1;
+            int right = n;
+            while(left<=right)
             {
-                if (s[i]==t[j])
+                int mid = (left+right)/2;
+                int x = (1+mid)*mid/2;
+                int y = (mid+n)*(n-mid+1)/2;
+                if (x==y) return mid;
+                else if (x<y)
                 {
-                    i++;
-                    j++;
+                    left = mid+1;
                 }
                 else
                 {
-                    i++;
+                    right= mid-1;
                 }
             }
-            return t.Length-j;
+            return -1;
         }
+
     }
 }
