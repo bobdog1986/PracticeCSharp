d3ee2b9c8b00a7044bc27a3f9e7594085dd2b230
bobdog1986 <bobdog1986@users.noreply.github.com>
2022-12-03 14:11:45 +0800

    2481. Minimum Cuts to Divide a Circle

--- Source/LeetCodeAlgo/AnswerA2000/Answer.A2450.cs
+++ Source/LeetCodeAlgo/AnswerA2000/Answer.A2450.cs
@@ -9,6 +9,58 @@ namespace LeetCodeAlgo
 {
     public partial class Answer
     {
+        ///2481. Minimum Cuts to Divide a Circle
+        //public int NumberOfCuts(int n)
+        //{
+        //    if (n==1) return 0;
+        //    if (n%2==0) return n/2;
+        //    return n;
+        //}
+
+        ///2485. Find the Pivot Integer
+        //return x that sum of [1,x] == [x,n], if not exist, return -1
+        //public int PivotInteger(int n)
+        //{
+        //    int left = 1;
+        //    int right = n;
+        //    while (left<=right)
+        //    {
+        //        int mid = (left+right)/2;
+        //        int x = (1+mid)*mid/2;
+        //        int y = (mid+n)*(n-mid+1)/2;
+        //        if (x==y) return mid;
+        //        else if (x<y)
+        //        {
+        //            left = mid+1;
+        //        }
+        //        else
+        //        {
+        //            right= mid-1;
+        //        }
+        //    }
+        //    return -1;
+        //}
+
+        ///2486. Append Characters to String to Make Subsequence
+        //Return the minimum chars that need to be appended to the end of s so that t becomes a subsequence of s.
+        //public int AppendCharacters(string s, string t)
+        //{
+        //    int i = 0, j = 0;
+        //    for(;i<s.Length && j<t.Length;)
+        //    {
+        //        if (s[i]==t[j])
+        //        {
+        //            i++;
+        //            j++;
+        //        }
+        //        else
+        //        {
+        //            i++;
+        //        }
+        //    }
+        //    return t.Length-j;
+        //}
+
         ///2487. Remove Nodes From Linked List
         //Remove every node which has a node with a strictly greater value anywhere to the right side of it.
         public ListNode RemoveNodes_2487(ListNode head)
@@ -40,7 +92,7 @@ namespace LeetCodeAlgo
             }
 
             var res = arr[0];
-            for(int j = 0; j<=i; j++)
+            for (int j = 0; j<=i; j++)
             {
                 arr[j].next = arr[j+1];
             }
@@ -64,50 +116,6 @@ namespace LeetCodeAlgo
             }
         }
 
-
-        ///2486. Append Characters to String to Make Subsequence
-        //Return the minimum chars that need to be appended to the end of s so that t becomes a subsequence of s.
-        //public int AppendCharacters(string s, string t)
-        //{
-        //    int i = 0, j = 0;
-        //    for(;i<s.Length && j<t.Length;)
-        //    {
-        //        if (s[i]==t[j])
-        //        {
-        //            i++;
-        //            j++;
-        //        }
-        //        else
-        //        {
-        //            i++;
-        //        }
-        //    }
-        //    return t.Length-j;
-        //}
-
-        ///2485. Find the Pivot Integer
-        //return x that sum of [1,x] == [x,n], if not exist, return -1
-        public int PivotInteger(int n)
-        {
-            int left = 1;
-            int right = n;
-            while(left<=right)
-            {
-                int mid = (left+right)/2;
-                int x = (1+mid)*mid/2;
-                int y = (mid+n)*(n-mid+1)/2;
-                if (x==y) return mid;
-                else if (x<y)
-                {
-                    left = mid+1;
-                }
-                else
-                {
-                    right= mid-1;
-                }
-            }
-            return -1;
-        }
 
     }
 }
