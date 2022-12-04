e56802f8c8785a4c79932ee97f5da0185babcb94
bobdog1986 <bobdog1986@users.noreply.github.com>
2022-12-04 14:27:28 +0800

    2466. Count Ways To Build Good Strings, #DP

--- Source/LeetCodeAlgo/AnswerA2000/Answer.A2450.cs
+++ Source/LeetCodeAlgo/AnswerA2000/Answer.A2450.cs
@@ -10,6 +10,24 @@ namespace LeetCodeAlgo
     public partial class Answer
     {
 
+        ///2466. Count Ways To Build Good Strings, #DP
+        public int CountGoodStrings(int low, int high, int zero, int one)
+        {
+            int[] dp = new int[high + 1];
+            int res = 0, mod = 1000000007;
+            dp[0] = 1;
+            for (int i = 1; i <= high; i++)
+            {
+                if (i >= zero)
+                    dp[i] = (dp[i] + dp[i - zero]) % mod;
+                if (i >= one)
+                    dp[i] = (dp[i] + dp[i - one]) % mod;
+                if (i >= low)
+                    res = (res + dp[i]) % mod;
+            }
+            return res;
+        }
+
         ///2471. Minimum Number of Operations to Sort a Binary Tree by Level
         //public int MinimumOperations(TreeNode root)
         //{
