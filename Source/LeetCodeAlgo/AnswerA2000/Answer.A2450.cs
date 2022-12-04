1fe00dc4a9d4739c9bd998a6fbff58e00dd72ea0
bobdog1986 <bobdog1986@users.noreply.github.com>
2022-12-04 17:07:14 +0800

    2475. Number of Unequal Triplets in Array

--- Source/LeetCodeAlgo/AnswerA2000/Answer.A2450.cs
+++ Source/LeetCodeAlgo/AnswerA2000/Answer.A2450.cs
@@ -75,6 +75,26 @@ namespace LeetCodeAlgo
         //    return res;
         //}
 
+        ///2475. Number of Unequal Triplets in Array
+        //public int UnequalTriplets(int[] nums)
+        //{
+        //    int res = 0;
+        //    int n = nums.Length;
+        //    for(int i = 0; i<n; i++)
+        //    {
+        //        for(int j = i+1; j<n; j++)
+        //        {
+        //            if (nums[i]==nums[j]) continue;
+        //            for(int k = j+1; k<n; k++)
+        //            {
+        //                if (nums[i]!=nums[k] && nums[j]!=nums[k])
+        //                    res++;
+        //            }
+        //        }
+        //    }
+        //    return res;
+        //}
+
         ///2481. Minimum Cuts to Divide a Circle
         //public int NumberOfCuts(int n)
         //{
