033d43d4943a86fccaa4ab5f275ffa478eb845fa
bobdog1986_Mac <bobdog1986@126.com>
2023-01-28 11:18:40 +0800

    2540. Minimum Common Value

---
+++ Source/LeetCodeAlgo/AnswerA2500/Answer.A2550.cs
@@ -0,0 +1,40 @@
+﻿using LeetCodeAlgo.Design;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeAlgo
+{
+	public partial class Answer
+	{
+        ///2540. Minimum Common Value
+        //public int GetCommon(int[] nums1, int[] nums2)
+        //{
+        //    int res = -1;
+        //    int i = 0;
+        //    int j = 0;
+        //    while (i < nums1.Length && j < nums2.Length)
+        //    {
+        //        if (nums1[i] == nums2[j])
+        //        {
+        //            res = nums1[i];
+        //            break;
+        //        }
+        //        else if (nums1[i] < nums2[j])
+        //        {
+        //            i++;
+        //        }
+        //        else
+        //        {
+        //            j++;
+        //        }
+        //    }
+        //    return res;
+        //}
+    }
+}
+
