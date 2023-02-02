812a298271013eea1da0dc8e06f1be9042844628
bobdog1986 <bobdog1986@users.noreply.github.com>
2023-02-02 22:17:38 +0800

    2550. Count Collisions of Monkeys on a Polygon

--- Source/LeetCodeAlgo/AnswerA2500/Answer.A2550.cs
+++ Source/LeetCodeAlgo/AnswerA2500/Answer.A2550.cs
@@ -11,7 +11,24 @@ namespace LeetCodeAlgo
 {
 	public partial class Answer
 	{
+        ///2550. Count Collisions of Monkeys on a Polygon
+        public int MonkeyMove(int n)
+        {
+            //res = 2^n - 2;
+            long res = 1;
+            long powBase = 2;
+            long mod = 1_000_000_007;
+            while (n > 0)
+            {
+                if (n % 2 == 1)
+                    res = res * powBase % mod;
+                powBase = powBase * powBase % mod;
+                n /= 2;
+            }
+            return (int)((res - 2 + mod) % mod);
+        }
 
-	}
+
+    }
 }
 
