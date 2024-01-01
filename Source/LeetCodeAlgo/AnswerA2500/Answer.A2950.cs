7e39b7cc9260d0aff7140222544bb1f3e83dfe79
bobdog1986 <bobdog1986@users.noreply.github.com>
2024-01-01 15:05:23 +0800

    2974. Minimum Number Game

---
+++ Source/LeetCodeAlgo/AnswerA2500/Answer.A2950.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeAlgo
+{
+    public partial class Answer
+    {
+        /// 2974. Minimum Number Game
+        //public int[] NumberGame(int[] nums)
+        //{
+        //    int len = nums.Length;
+        //    int[] arr = new int[len];
+        //    Array.Sort(nums);
+        //    for (int i = 0; i<len; i+=2)
+        //    {
+        //        arr[i+1]=nums[i];
+        //        arr[i]=nums[i+1];
+        //    }
+        //    return arr;
+        //}
+
+    }
+}
