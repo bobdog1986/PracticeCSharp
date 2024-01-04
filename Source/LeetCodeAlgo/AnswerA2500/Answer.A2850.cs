cfc039e9f01d8ed0513c46c3ddeb49150ddb7556
bobdog1986 <bobdog1986@users.noreply.github.com>
2024-01-05 00:31:51 +0800

    2870. Minimum Number of Operations to Make Array Empty

---
+++ Source/LeetCodeAlgo/AnswerA2500/Answer.A2850.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeAlgo
+{
+    public partial class Answer
+    {
+        ///2870. Minimum Number of Operations to Make Array Empty
+        //public int MinOperations_2870(int[] nums)
+        //{
+        //    var dict = new Dictionary<int, int>();
+        //    foreach (var i in nums)
+        //    {
+        //        if (dict.ContainsKey(i)) dict[i]++;
+        //        else dict.Add(i, 1);
+        //    }
+        //    int res = 0;
+        //    foreach (var k in dict.Keys)
+        //    {
+        //        if (dict[k]==1)
+        //            return -1;
+        //        else
+        //        {
+        //            if (dict[k]%3==0)
+        //                res+=dict[k]/3;
+        //            else
+        //                res+=dict[k]/3+1;
+        //        }
+        //    }
+        //    return res;
+        //}
+    }
+}
