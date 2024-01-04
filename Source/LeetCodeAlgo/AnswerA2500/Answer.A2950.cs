6cd120ea848406d0487071f5ea8581ff9269e48c
bobdog1986 <bobdog1986@users.noreply.github.com>
2024-01-05 00:34:11 +0800

    2981. Find Longest Special Substring That Occurs Thrice I

--- Source/LeetCodeAlgo/AnswerA2500/Answer.A2950.cs
+++ Source/LeetCodeAlgo/AnswerA2500/Answer.A2950.cs
@@ -22,5 +22,45 @@ namespace LeetCodeAlgo
         //    return arr;
         //}
 
+        ///2981. Find Longest Special Substring That Occurs Thrice I
+        //public int MaximumLength(string s)
+        //{
+        //    Dictionary<char, Dictionary<int, int>> dict = new Dictionary<char, Dictionary<int, int>>();
+        //    int res = 0;
+        //    int len = 0;
+        //    char c = ' ';
+        //    for (int i = 0; i<s.Length; i++)
+        //    {
+        //        if (s[i]==c)
+        //        {
+        //            len++;
+        //        }
+        //        else
+        //        {
+        //            c=s[i];
+        //            len=1;
+        //        }
+        //        if (!dict.ContainsKey(c))
+        //            dict.Add(c, new Dictionary<int, int>());
+        //        if (!dict[c].ContainsKey(len))
+        //            dict[c].Add(len, 0);
+
+        //        for (int j = 1; j<=len; j++)
+        //            dict[c][j]++;
+        //    }
+
+
+        //    foreach (var i in dict.Values)
+        //    {
+        //        foreach (var k in i.Keys)
+        //        {
+        //            if (i[k]>=3)
+        //            {
+        //                res=Math.Max(res, k);
+        //            }
+        //        }
+        //    }
+        //    return res==0 ? -1 : res;
+        //}
     }
 }
