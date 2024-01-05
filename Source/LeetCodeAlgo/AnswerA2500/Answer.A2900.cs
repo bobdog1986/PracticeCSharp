62d428a159b3f41767ee5e17d47c80a93052d7fb
bobdog1986 <bobdog1986@users.noreply.github.com>
2024-01-06 00:12:47 +0800

    2938. Separate Black and White Balls

---
+++ Source/LeetCodeAlgo/AnswerA2500/Answer.A2900.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeAlgo
+{
+    public partial class Answer
+    {
+        ///2938. Separate Black and White Balls
+        //public long MinimumSteps(string s)
+        //{
+        //    int ones = 0;
+        //    long res = 0;
+        //    foreach (var i in s)
+        //    {
+        //        if (i=='0')
+        //        {
+        //            res+=ones;
+        //        }
+        //        else ones++;
+        //    }
+        //    return res;
+        //}
+    }
+}
