cfae6d8c2e78d2d638675714a7a38b873f273d0d
bobdog1986 <bobdog1986@users.noreply.github.com>
2024-01-09 00:27:49 +0800

    2944. Minimum Number of Coins for Fruits, #DP

--- Source/LeetCodeAlgo/AnswerA2500/Answer.A2900.cs
+++ Source/LeetCodeAlgo/AnswerA2500/Answer.A2900.cs
@@ -23,5 +23,23 @@ namespace LeetCodeAlgo
         //    }
         //    return res;
         //}
+
+        ///2944. Minimum Number of Coins for Fruits, #DP
+        //public int MinimumCoins(int[] prices)
+        //{
+        //    int n = prices.Length;
+        //    int[] dp = new int[n+1];
+        //    Array.Fill(dp, int.MaxValue);
+        //    dp[0]=0;
+        //    for (int i = 1; i<=n; i++)
+        //    {
+        //        for (int j = i; j<=i*2 && j<=n; j++)
+        //        {
+        //            dp[j] = Math.Min(dp[j], dp[i-1]+prices[i-1]);
+        //        }
+        //    }
+
+        //    return dp[n];
+        //}
     }
 }
