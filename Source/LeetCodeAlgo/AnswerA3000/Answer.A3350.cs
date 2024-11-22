9551b219076bae22fd13b0a3d47e65fac63b341b
bobdog1986 <bobdog1986@users.noreply.github.com>
2024-11-22 23:26:08 +0800

    3354. Make Array Elements Equal to Zero

---
+++ Source/LeetCodeAlgo/AnswerA3000/Answer.A3350.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeAlgo
+{
+    public partial class Answer
+    {
+        ///3354. Make Array Elements Equal to Zero
+        public int CountValidSelections(int[] nums)
+        {
+            int res = 0;
+            int total = nums.Sum();
+            int curr = 0;
+            for (int i = 0; i<nums.Length; i++)
+            {
+                curr+=nums[i];
+                if (nums[i]==0)
+                {
+                    if (curr*2 == total)
+                    {
+                        res+=2;
+                    }
+                    else if (curr*2 == total+1 ||curr*2 == total-1)
+                    {
+                        res++;
+                    }
+                }
+            }
+            return res;
+        }
+    }
+}
