779b3c004d801fb06d0be70d19909ab9bdd86d97
bobdog1986 <bobdog1986@users.noreply.github.com>
2025-01-13 22:32:22 +0800

    3223. Minimum Length of String After Operations

---
+++ Source/LeetCodeAlgo/AnswerA3000/Answer.A3200.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeAlgo
+{
+    public partial class Answer
+    {
+        ///3223. Minimum Length of String After Operations
+        //public int MinimumLength(string s)
+        //{
+        //    int res = 0;
+        //    int[] arr = new int[26];
+        //    foreach (var c in s)
+        //    {
+        //        arr[c-'a']++;
+        //    }
+        //    foreach (var i in arr)
+        //    {
+        //        if (i>=3)
+        //        {
+        //            if (i%2==1) res++;
+        //            else res+=2;
+        //        }
+        //        else res+=i;
+        //    }
+        //    return res;
+        //}
+
+
+    }
+}
