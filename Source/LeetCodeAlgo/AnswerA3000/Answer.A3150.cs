b5ef52a6db79da52c8e37cb9f3dca7e8b8239eae
bobdog1986 <bobdog1986@users.noreply.github.com>
2025-02-04 20:18:46 +0800

    3151. Special Array I

---
+++ Source/LeetCodeAlgo/AnswerA3000/Answer.A3150.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeAlgo
+{
+    public partial class Answer
+    {
+        ///3151. Special Array I
+        //public bool IsArraySpecial(int[] nums)
+        //{
+        //    for (int i = 1; i<nums.Length; i++)
+        //    {
+        //        if (nums[i]%2 == nums[i-1]%2) return false;
+        //    }
+        //    return true;
+        //}
+    }
+}
