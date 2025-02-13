95e3f13ff1f0b8f5b61826ebc96923ce8618fe15
bobdog1986 <bobdog1986@users.noreply.github.com>
2025-02-13 22:31:18 +0800

    3066. Minimum Operations to Exceed Threshold Value II, #PriorityQueue

---
+++ Source/LeetCodeAlgo/AnswerA3000/Answer.A3050.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeAlgo
+{
+    public partial class Answer
+    {
+        ///3066. Minimum Operations to Exceed Threshold Value II, #PriorityQueue
+        //public int MinOperations_3066(int[] nums, int k)
+        //{
+        //    int res = 0;
+        //    var pq = new PriorityQueue<long, long>();
+        //    foreach (var i in nums)
+        //    {
+        //        pq.Enqueue(i, i);
+        //    }
+        //    while (pq.Count>=2 &&pq.Peek()<k)
+        //    {
+        //        res++;
+        //        long a = pq.Dequeue();
+        //        long b = pq.Dequeue();
+        //        pq.Enqueue(a*2+b, a*2+b);
+        //    }
+        //    return res;
+        //}
+    }
+}
