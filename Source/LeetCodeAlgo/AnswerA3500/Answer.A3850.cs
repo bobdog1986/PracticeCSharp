d5ac65ad06c934a27bc797d28a34a70896d5a20e
bobdog1986 <bobdog1986@users.noreply.github.com>
2026-09-06 10:46:00 +0800

    3875, 3876. Construct Uniform Parity Array II

---
+++ Source/LeetCodeAlgo/AnswerA3500/Answer.A3850.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeAlgo
+{
+    public partial class Answer
+    {
+
+        ///3875. Construct Uniform Parity Array I
+        ///3876. Construct Uniform Parity Array II
+        //public bool UniformArray(int[] nums1)
+        //{
+        //    Array.Sort(nums1);
+        //    int n = nums1.Length;
+
+        //    int[] arr = new int[n];
+        //    int odds = 0;
+        //    for (int i = 0; i<n; i++)
+        //    {
+        //        arr[i]=odds;
+        //        if (nums1[i]%2!=0)
+        //        {
+        //            odds++;
+        //        }
+        //    }
+        //    if (odds ==n ||odds==0) return true;
+        //    for (int i = 0; i<n; i++)
+        //    {
+        //        if (nums1[i]%2==0)
+        //        {
+        //            if (arr[i]==0) return false;
+        //        }
+        //    }
+        //    return true;
+        //}
+
+    }
+}
